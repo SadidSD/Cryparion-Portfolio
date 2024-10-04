908af2bebc1f2e33d74b5a46a1927c22fc482c30
SadidSD <122972806+SadidSD@users.noreply.github.com>
2024-10-04 21:16:36 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openpage(pagename){
+    const page= document.getElementById(pagename)
+    page.style.display= "flex"  
+}
+
+function closepage(pagename){
+    const page= document.getElementById(pagename)
+    page.style.display= 'none'
+    }
+
